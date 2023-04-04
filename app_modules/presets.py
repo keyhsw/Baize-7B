46db63f7db7a6b9ab5b4d7d369151554706e259b
Baize <project-baize@users.noreply.huggingface.co>
2023-04-04 06:32:09 +0000

    Update app_modules/presets.py

--- app_modules/presets.py
+++ app_modules/presets.py
@@ -4,8 +4,13 @@ import gradio as gr
 
 title = """<h1 align="left" style="min-width:200px; margin-top:0;"> <img src="https://raw.githubusercontent.com/twitter/twemoji/master/assets/svg/1f432.svg" width="32px" style="display: inline"> Baize </h1>"""
 description_top = """\
-<div align="left" style="margin:16px 0">
+<div align="left">
+<p>
+If there're too many people waiting in the queue, feel free to run the demo locally following our instructions <a href=" ">here</a >.
+</p >
+<p>
 Disclaimer: The LLaMA model is a third-party version available on Hugging Face model hub. This demo should be used for research purposes only. Commercial use is strictly prohibited. The model output is not censored and the authors do not endorse the opinions in the generated content. Use at your own risk.
+</p >
 </div>
 """
 description = """\
